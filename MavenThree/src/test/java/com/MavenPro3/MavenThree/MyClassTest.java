8a6a9f85a97eb7d2251fa93fb1cd262603c38f3a
mukul <mukul@192.168.0.101>
2019-02-13 11:46:54 +0530

    mavenproject

---
+++ MavenThree/src/test/java/com/MavenPro3/MavenThree/MyClassTest.java
@@ -0,0 +1,13 @@
+package com.MavenPro3.MavenThree;
+
+import org.testng.annotations.Test;
+
+public class MyClassTest {
+	@Test
+	
+	public void openBrowser()
+	{
+		System.out.println("*********************");
+	}
+
+}
